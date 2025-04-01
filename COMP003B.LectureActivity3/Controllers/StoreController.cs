b5237082ee08bf468e2e5d87027097ebeacf1be5
adodoCoder <alavropulido@my.whccd.edu>
2025-03-31 18:48:28 -0700

    added final files to controller folder

---
+++ COMP003B.LectureActivity3/Controllers/StoreController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.LectureActivity3.Controllers
+{
+    // The StoreController class is a controller that handles requests for the store section of the website.
+    // The [Route("storage")] attribute specifies that the controller will handle requests that start with "/store".
+    [Route("store")]
+
+    public class StoreController : Controller
+    {
+        // Get: store/product/5
+        [Route("product/{id:int}")]
+
+        public IActionResult ViewProduct(int id)
+        {
+            // The View() method returns a view result that renders a view to the response.
+            // The id parameter is passed to the view.
+            return View();
+        }
+
+        // Get : store/product/search?category=electronics
+        [HttpGet("product/search")]
+
+        public IActionResult Search([FromQuery] string category)
+        {
+            return View("Search", category);
+        }
+    }
+}
